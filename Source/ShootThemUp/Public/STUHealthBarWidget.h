3a3975e65989f191022ece9970f99fa8ba903743
Дмитрий Золотарев <dmitry.v.zolotarev@mail.ru>
2023-06-27 13:30:39 +0700

    Полоса здоровья у неписей

---
+++ Source/ShootThemUp/Public/STUHealthBarWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "STUHealthBarWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API USTUHealthBarWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
